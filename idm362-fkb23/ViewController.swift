b123aa9ec36b82587980b9b5709497764c33949c
Fidel Boamah <fkb23@drexel.edu>
2021-03-08 23:57:16 -0500

    Launch screen and logging button taps

--- idm362-fkb23/ViewController.swift
+++ idm362-fkb23/ViewController.swift
@@ -77,7 +77,83 @@ class ViewController: UIViewController {
 
     }
     
+    var D3 = false
+    var D4 = false
+    var S3 = false
+    var S4 = false
+    var samescore = 0
+    var diffscore = 0
     
-
+    // Different buttons
+    @IBAction func D1(_ sender: Any) {
+        print("D1 pressed")
+        
+    }
+    
+    
+    @IBAction func D2(_ sender: Any) {
+        print("D2 pressed")
+    }
+    
+    @IBAction func D3(_ sender: Any) {
+        D3 = true
+        print("D3 pressed, its value is \(D3)")
+    }
+    
+    @IBAction func D4(_ sender: Any) {
+        D4 = true
+        print("D4 pressed, its value is \(D4)")
+    }
+    
+    
+    // Same buttons
+    @IBAction func S1(_ sender: Any) {
+        print("S1 pressed")
+    }
+    
+    @IBAction func S2(_ sender: Any) {
+        print("S2 pressed")
+    }
+    
+    @IBAction func S3(_ sender: Any) {
+        S3 = true
+        print("S3 pressed, its value is \(S3)")
+    }
+    
+    @IBAction func S4(_ sender: Any) {
+        S4 = true
+        print("S4 pressed, its value is \(S4)")
+    }
+    
+    func winner() {
+        if (D3 == true && S3 == true) {
+            print("same wins")
+            samescore = samescore + 1
+        }
+        
+        else if (D4 == true && S4 == true) {
+            print ("same wins")
+            samescore = samescore + 1
+        }
+        
+        else if (D3 == true && S4 == true) {
+            print ("different wins")
+            diffscore = diffscore + 1
+        }
+        
+        else if (D4 == true && S3 == true) {
+            print ("Different wins")
+            diffscore = diffscore + 1
+        }
+        
+        D3 = false
+        D4 = false
+        S3 = false
+        S4 = false
+        
+    }
+    
+    
+    
 } // viewcontroller end
 
